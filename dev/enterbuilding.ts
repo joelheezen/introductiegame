58fc8bc85e0b0c1d63bb8d43ad31d392eebaea21
luuksg12 <0966746@hr.nl>
2020-07-09 10:20:00 +0200

    Pull from Joel

--- dev/enterbuilding.ts
+++ dev/enterbuilding.ts
@@ -1,9 +1,14 @@
+window.addEventListener("load", () => new EnterBuilding())
 class EnterBuilding{
+
+    protected game = document.getElementsByTagName('game')[0]
+
     constructor(){
-        
+        this.setBackground()
     }
     setBackground(){
         let bge = document.createElement("backgroundenter")
-        bge.style.backgroundImage = 'url(ass'
+        bge.style.backgroundImage = 'url(assets\IMG_20200708_123510.jpg)'
+        this.game.appendChild(bge)
     }
 }
