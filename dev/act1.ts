6aa765c5bae41ee508c426beb2299e342f23af14
joel <joel_heezen96@hotmail.com>
2020-07-09 11:24:46 +0200

    added buttons with 80% opacity later this will be 0%

--- dev/act1.ts
+++ dev/act1.ts
@@ -1,14 +1,33 @@
-class Act1{
+class Act1 {
 
 
 
-    constructor(){
+    constructor() {
         let game = document.getElementsByTagName("game")[0]
         let background = document.createElement("backgroundact1")
         game.appendChild(background)
-        let button = document.createElement("button")
-        game.appendChild(button)
+        let button1 = document.createElement("button")
+        game.appendChild(button1)
+        button1.style.width = `15vw`
+        button1.style.height = `7.5vh`
+        button1.style.transform = `translate(42vw, 47vh)`
+        button1.style.opacity = `80%`
+        button1.onclick = this.shapeCheck
+        let button2 = document.createElement("button")
+        game.appendChild(button2)
+        button2.style.width = `18vw`
+        button2.style.height = `3vh`
+        button2.style.transform = `translate(25vw, 60vh)`
+        button2.style.opacity = `80%`
+        button2.onclick = this.searchOnline
     }
 
+    shapeCheck() {
+        console.log("button 1 is pressed")
+    }
+
+    searchOnline() {
+        console.log("button 2 is pressed")
+    }
 
 }
--- docs/js/main.js
+++ docs/js/main.js
@@ -4,9 +4,27 @@ var Act1 = (function () {
         var game = document.getElementsByTagName("game")[0];
         var background = document.createElement("backgroundact1");
         game.appendChild(background);
-        var button = document.createElement("button");
-        game.appendChild(button);
+        var button1 = document.createElement("button");
+        game.appendChild(button1);
+        button1.style.width = "15vw";
+        button1.style.height = "7.5vh";
+        button1.style.transform = "translate(42vw, 47vh)";
+        button1.style.opacity = "80%";
+        button1.onclick = this.shapeCheck;
+        var button2 = document.createElement("button");
+        game.appendChild(button2);
+        button2.style.width = "18vw";
+        button2.style.height = "3vh";
+        button2.style.transform = "translate(25vw, 60vh)";
+        button2.style.opacity = "80%";
+        button2.onclick = this.searchOnline;
     }
+    Act1.prototype.shapeCheck = function () {
+        console.log("button 1 is pressed");
+    };
+    Act1.prototype.searchOnline = function () {
+        console.log("button 2 is pressed");
+    };
     return Act1;
 }());
 var EnterBuilding = (function () {
