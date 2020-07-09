521af7f95cbfd49aa8d245cc5fe1e5cd735a3ffb
Esolutions-IT <43686864+Esolutions-IT@users.noreply.github.com>
2020-07-09 11:24:58 +0200

    Act1 invoerveld
    Act1 invoervelden vormen

--- dev/act1.ts
+++ dev/act1.ts
@@ -4,10 +4,24 @@ class Act1{
 
     constructor(){
         let game = document.getElementsByTagName("game")[0]
+
         let background = document.createElement("backgroundact1")
         game.appendChild(background)
+
         let button = document.createElement("button")
         game.appendChild(button)
+
+        let input1 = document.createElement("input")
+        game.appendChild(input1)
+        input1.style.width = `13.8vw`
+        input1.style.height = `5.7vh`
+        input1.style.transform = `translate(41.2vw, 38.4vh)`
+        input1.style.borderRadius = `25px`
+        input1.style.border = `none`
+        input1.style.fontSize = `20px`
+        input1.style.paddingLeft = `20px`
+        input1.style.fontFamily = `Arial Black`
+        input1.style.textTransform = `uppercase`
     }
 
 
--- docs/js/main.js
+++ docs/js/main.js
@@ -6,6 +6,17 @@ var Act1 = (function () {
         game.appendChild(background);
         var button = document.createElement("button");
         game.appendChild(button);
+        var input1 = document.createElement("input");
+        game.appendChild(input1);
+        input1.style.width = "13.8vw";
+        input1.style.height = "5.7vh";
+        input1.style.transform = "translate(41.2vw, 38.4vh)";
+        input1.style.borderRadius = "25px";
+        input1.style.border = "none";
+        input1.style.fontSize = "20px";
+        input1.style.paddingLeft = "20px";
+        input1.style.fontFamily = "Arial Black";
+        input1.style.textTransform = "uppercase";
     }
     return Act1;
 }());
