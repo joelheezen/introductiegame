2e135169887be6050e1cfec0c910518be40f097e
luuksg12 <0966746@hr.nl>
2020-07-09 10:08:10 +0200

    added assets

---
+++ dev/enterbuilding.ts
@@ -0,0 +1,9 @@
+class EnterBuilding{
+    constructor(){
+        
+    }
+    setBackground(){
+        let bge = document.createElement("backgroundenter")
+        bge.style.backgroundImage = 'url(ass'
+    }
+}
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,14 @@
 "use strict";
+var EnterBuilding = (function () {
+    function EnterBuilding() {
+        this.bg1 = document.createElement("backgroundenter1");
+        this.bg2 = document.createElement("backgroundenter2");
+        this.bg3 = document.createElement("backgroundenter3");
+    }
+    EnterBuilding.prototype.setBackground = function () {
+    };
+    return EnterBuilding;
+}());
 var test = document.createElement("test");
 var game = document.getElementsByTagName("game")[0];
 game.appendChild(test);
