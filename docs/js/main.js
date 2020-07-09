73b6eb89f22b0308e156920dc00715919de0b05a
joel <joel_heezen96@hotmail.com>
2020-07-09 10:14:19 +0200

    added act1 class

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
-var test = document.createElement("test");
-var game = document.getElementsByTagName("game")[0];
-game.appendChild(test);
+var Act1 = (function () {
+    function Act1() {
+        var game = document.getElementsByTagName("game")[0];
+        var background = document.createElement("backgroundact1");
+        game.appendChild(background);
+        var button = document.createElement("button");
+        game.appendChild(button);
+    }
+    return Act1;
+}());
+var Game = (function () {
+    function Game() {
+        new Act1();
+    }
+    return Game;
+}());
+window.addEventListener("load", function () { return new Act1(); });
 //# sourceMappingURL=main.js.map
