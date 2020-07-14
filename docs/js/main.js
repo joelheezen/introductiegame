afd13449b7e41d806248791526bbe6524bc595b3
Tom Faust <tom.faust@hotmail.com>
2020-07-14 19:08:42 +0200

    select educations in alphabetical order

--- docs/js/main.js
+++ docs/js/main.js
@@ -1689,6 +1689,7 @@ var Ending = (function () {
         button.addEventListener("click", function () {
             window.print();
         });
+        new Timer().resetTimer();
     }
     return Ending;
 }());
@@ -1802,14 +1803,18 @@ var LocatieSelectie = (function () {
         this.game.appendChild(this.educationSet);
         this.educationSet.innerHTML = "Om van start te gaan moeten we weten aan welke opleiding jij deel neemt. Kies uit deze lijst jouw opleiding.";
         this.educationSelect = document.createElement('select');
-        for (var index = 0; index < this.educations.length; index++) {
-            this.currentLocation = this.educations[index].opleidingen;
-            for (var education in this.currentLocation) {
-                var addToDrop = document.createElement('option');
-                addToDrop.value = this.currentLocation[education];
-                addToDrop.innerHTML = this.currentLocation[education];
-                this.educationSelect.appendChild(addToDrop);
+        var allEducations = new Array();
+        for (var index1 = 0; index1 < this.educations.length; index1++) {
+            for (var index2 = 0; index2 < this.educations[index1].opleidingen.length; index2++) {
+                allEducations.push(this.educations[index1].opleidingen[index2]);
             }
+        }
+        allEducations = allEducations.sort();
+        for (var education in allEducations) {
+            var addToDrop = document.createElement('option');
+            addToDrop.value = allEducations[education];
+            addToDrop.innerHTML = allEducations[education];
+            this.educationSelect.appendChild(addToDrop);
         }
         this.educationSet.appendChild(this.educationSelect);
         var thisLocation = document.createElement('button');
@@ -2070,9 +2075,7 @@ var Locations = (function () {
         };
         this.wijnhaven_103 = {
             location: "wijnhaven_103",
-            opleidingen: [
-                ""
-            ],
+            opleidingen: [],
             locatieInfo: "https://www.hogeschoolrotterdam.nl/hogeschool/locaties/wijnhaven-103/"
         };
         this.wijnhaven_107 = {
