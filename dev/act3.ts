d5c3fc6c66276d809f2cdb0838b3457697c85902
joel <joel_heezen96@hotmail.com>
2020-07-23 11:35:29 +0200

    fixed char sizes

--- dev/act3.ts
+++ dev/act3.ts
@@ -399,7 +399,7 @@ class Act3{
                 char1.style.backgroundImage = `url(assets/Akte3/cees.png)`
                 char1.style.backgroundSize = `100% 100%`
                 char1.style.width = `3.5vw`
-                char1.style.height = `10vh`
+                char1.style.height = `14vh`
                 char1.style.position = `absolute`
                 char1.style.transform = `translate(42.5vw, 33vh)`
                 char1.addEventListener("mouseover", function(){
@@ -419,7 +419,7 @@ class Act3{
                 char2.style.backgroundImage = `url(assets/Akte3/werner.png)`
                 char2.style.backgroundSize = `100% 100%`
                 char2.style.width = `3.5vw`
-                char2.style.height = `10vh`
+                char2.style.height = `14vh`
                 char2.style.position = `absolute`
                 char2.style.transform = `translate(29.5vw, 50vh)`
                 char2.addEventListener("mouseover", function(){
@@ -453,7 +453,7 @@ class Act3{
                 char3.style.backgroundImage = `url(assets/Akte3/loes.png)`
                 char3.style.backgroundSize = `100% 100%`
                 char3.style.width = `3.5vw`
-                char3.style.height = `10vh`
+                char3.style.height = `14vh`
                 char3.style.position = `absolute`
                 char3.style.transform = `translate(37.6vw, 40.3vh)`
                 char3.addEventListener("mouseover", function(){
@@ -473,7 +473,7 @@ class Act3{
                 char4.style.backgroundImage = `url(assets/Akte3/donna.png)`
                 char4.style.backgroundSize = `100% 100%`
                 char4.style.width = `3.5vw`
-                char4.style.height = `10vh`
+                char4.style.height = `14vh`
                 char4.style.position = `absolute`
                 char4.style.transform = `translate(40.5vw, 39vh)`
                 char4.addEventListener("mouseover", function(){
@@ -493,7 +493,7 @@ class Act3{
                 char5.style.backgroundImage = `url(assets/Akte3/zoe.png)`
                 char5.style.backgroundSize = `100% 100%`
                 char5.style.width = `3.5vw`
-                char5.style.height = `10vh`
+                char5.style.height = `14vh`
                 char5.style.position = `absolute`
                 char5.style.transform = `translate(20.5vw, 42vh)`
                 char5.addEventListener("mouseover", function(){
